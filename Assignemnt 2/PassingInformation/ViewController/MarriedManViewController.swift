22cdfd5ce9ca4bb3b5901b53468ae6d9d3c1e4ae
Cong Le <cle155@csu.fullerton.edu>
2020-01-27 12:38:39 -0500

    assignment2
    passing information assignment using delegation and notification communicatin patterns

---
+++ Assignemnt 2/PassingInformation/ViewController/MarriedManViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MarriedManViewController.swift
+//  PassingInformation
+//
+//  Created by Cong Le on 1/26/20.
+//  Copyright © 2020 EnhanceIT. All rights reserved.
+//
+
+import UIKit
+
+class MarriedManViewController: UIViewController {
+    
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var marriedManImage: UIImageView!
+    
+    @IBOutlet weak var marriedManLabel: UILabel!
+    
+    @IBOutlet weak var yesKidButton: UIButton!
+    
+    
+    @IBOutlet weak var noKidButton: UIButton!
+    
+    
+    //MARK: Properties
+    var message: String! = "connected marriedVC VC via segue"
+    
+    
+    //MARK: VC Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    //MARK: IBAction
+    
+    
+    @IBAction func yesKidButtonTapped(_ sender: UIButton) {
+        print("just tapped yes Kid Button ")
+        
+    }
+    
+    
+    
+    @IBAction func noKidButtonTapped(_ sender: UIButton) {
+        print("Just tapped no Kid Button ")
+        let notifyName = Notification.Name(noKidNotificationKey)
+        
+        //Notification - 2 - post the notification for the listeners
+        NotificationCenter.default.post(name: notifyName, object: nil)
+     
+        let mainVC = storyboard?.instantiateViewController(identifier: "MainVC") as! MainViewController
+               
+         present(mainVC, animated: true, completion: nil)
+    }
+    
+    
+    
+    //MARK: Function
+  
+}
